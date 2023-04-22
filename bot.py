5e3fb01c45d189b0279799d4dafef705e67c4cba
Nikita Sharma <123251896+sktvblogger@users.noreply.github.com>
2023-04-22 10:09:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,6 +39,6 @@ async def get_shortlink(link):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params, raise_for_status=True) as response:
             data = await response.json()
-            return file_code = data['result']['file_code']
+            return data['result']['file_code']
 
 bot.run()
